ecd5527766adf3d5db548d291b7b991f2745608a
simonholt-flyinglizard <simonvholt92@gmail.com>
2025-11-25 10:44:24 +0000

    height in pokeList converted to cm for ease

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 let pokemonList = [{
     name: "Bulbasaur",
-    height: `2' 04"`, 
+    heightCm: 71, 
     type: "grass",
     poisionous: true,
     weaknesses: ["Fire", "Ice", "Flying", "Psychic"]
   }, {
     name: "Charmander ",
-    height: `2' 00"`,
+    heightCm: 61,
     type: "fire",
     poisionous: false,
     weaknesses: ["Water", "Ground", "Rock"]
   }, {
     name: "Squirtle",
-    height: `1' 08"`,
+    heightCm: 50,
     type: "water",
     poisionous: false,
     weaknesses: ["Grass", "Electric"]
   }, {
     name: "Diglett",
-    height: `0' 08"`,
+    heightCm: 20,
     type: "ground",
     poisionous: false,
     weaknesses: ["Water", "Grass", "Ice"]
@@ -29,4 +29,13 @@ let currentPokemonIndex = 1;
 
 console.log("the current pokemon is " + pokemonList[currentPokemonIndex].name +
             "a " + pokemonList[currentPokemonIndex].type + " type pokemon, that is " +
-            pokemonList[currentPokemonIndex].height + " tall.");
+            pokemonList[currentPokemonIndex].heightCm + " tall.");
+            
+
+pokemonList.for
+
+for (let i = 0; i <= pokemonList.length; i++) {
+  console.log(i);
+
+  document.write(pokemonList[i].name + "/");
+}
