57ca367b19e74cfc0209a4e9af37d42c19e0a02c
simonholt-flyinglizard <simonvholt92@gmail.com>
2025-11-25 10:33:03 +0000

    Improved formatting for pokemon object array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-let pokemonList = [{name: "Bulbasaur", height: `2' 04"`, type: "grass", poisionous: true,
-                weaknesses: ["Fire", "Ice", "Flying", "Psychic"]},
-                {name: "Charmander ", height: `2' 00"`, type: "fire", poisionous: false,
-                weaknesses: ["Water", "Ground", "Rock"]},
-                {name: "Squirtle", height: `1' 08"`, type: "water", poisionous: false,
-                weaknesses: ["Grass", "Electric"]},
-                {name: "Diglett", height: `0' 08"`, type: "ground", poisionous: false,
-                weaknesses: ["Water", "Grass", "Ice"]},
+let pokemonList = [{
+    name: "Bulbasaur",
+    height: `2' 04"`, 
+    type: "grass",
+    poisionous: true,
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"]
+  }, {
+    name: "Charmander ",
+    height: `2' 00"`,
+    type: "fire",
+    poisionous: false,
+    weaknesses: ["Water", "Ground", "Rock"]
+  }, {
+    name: "Squirtle",
+    height: `1' 08"`,
+    type: "water",
+    poisionous: false,
+    weaknesses: ["Grass", "Electric"]
+  }, {
+    name: "Diglett",
+    height: `0' 08"`,
+    type: "ground",
+    poisionous: false,
+    weaknesses: ["Water", "Grass", "Ice"]
+  }
 ]
 
 let currentPokemonIndex = 1;
