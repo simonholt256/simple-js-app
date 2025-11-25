7e6e4230d4f6af0363dcefe89e81f80cb00a8902
simonholt-flyinglizard <simonvholt92@gmail.com>
2025-11-25 10:20:40 +0000

    Object array of pokemon created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
-alert('Hello world');
+let pokemonList = [{name: "Bulbasaur", height: `2' 04"`, type: "grass", poisionous: true,
+                weaknesses: ["Fire", "Ice", "Flying", "Psychic"]},
+                {name: "Charmander ", height: `2' 00"`, type: "fire", poisionous: false,
+                weaknesses: ["Water", "Ground", "Rock"]},
+                {name: "Squirtle", height: `1' 08"`, type: "water", poisionous: false,
+                weaknesses: ["Grass", "Electric"]},
+                {name: "Diglett", height: `0' 08"`, type: "ground", poisionous: false,
+                weaknesses: ["Water", "Grass", "Ice"]},
+]
 
-let favouriteFood = "Pizza";
+let currentPokemonIndex = 1;
 
-document.open()
-
-document.write(favouriteFood);
-
-document.close()
-
-console.log(favouriteFood);
+console.log("the current pokemon is " + pokemonList[currentPokemonIndex].name +
+            "a " + pokemonList[currentPokemonIndex].type + " type pokemon, that is " +
+            pokemonList[currentPokemonIndex].height + " tall.");
