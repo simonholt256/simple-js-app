2a51d80bd9408ce9d213112a3872c520378873df
simonholt-flyinglizard <simonvholt92@gmail.com>
2025-12-09 09:43:08 +0000

    polyfill added

--- js/scripts.js
+++ js/scripts.js
@@ -122,3 +122,12 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon)
 
 });
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+  return response.json(); // This returns a promise!
+}).then(function (pokemonList) {
+  console.log(pokemonList); // The actual JSON response
+}).catch(function () {
+  // Error
+});
